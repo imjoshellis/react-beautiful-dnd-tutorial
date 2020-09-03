3496104ea17116b538804361a2e723d315359a73
Josh Ellis <josh@imjoshellis.com>
2020-09-03 17:38:58 -0500

    Add DragDropContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import Column from './components/Column'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 function App () {
   const list = ['Item 1', 'Item 2', 'Item 3']
+
+  const onDragEnd = () => null
+
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr',
-        margin: '24px auto',
-        width: '80%',
-        gap: '8px'
-      }}
-    >
-      <Column items={list} />
-    </div>
+    <DragDropContext onDragEnd={() => onDragEnd()}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          margin: '24px auto',
+          width: '80%',
+          gap: '8px'
+        }}
+      >
+        <Column items={list} />
+      </div>
+    </DragDropContext>
   )
 }
 
