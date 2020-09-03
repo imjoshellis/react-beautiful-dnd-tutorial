4b12a479749869a233942589109036dc6bb92761
Josh Ellis <josh@imjoshellis.com>
2020-09-03 18:53:50 -0500

    Add onDragEnd for single column

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Column from './components/Column'
-import { DragDropContext } from 'react-beautiful-dnd'
+import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 
 function App () {
-  const list = ['Item 1', 'Item 2', 'Item 3']
+  const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3'])
 
-  const onDragEnd = () => null
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    // Make sure we have a valid destination
+    if (destination === undefined || destination === null) return null
+
+    // Make sure we're actually moving the item
+    if (destination.index === source.index) return null
+
+    // Move the item within the list
+    // Start by making a new list without the dragged item
+    const newList = list.filter((_: any, idx: number) => idx !== source.index)
+
+    // Then insert the item at the right location
+    newList.splice(destination.index, 0, list[source.index])
+
+    // Update the list
+    setList(newList)
+  }
 
   return (
-    <DragDropContext onDragEnd={() => onDragEnd()}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div
         style={{
           display: 'grid',
