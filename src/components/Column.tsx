71fd657ee8424f96ee9cf0c10f08bddedc26b4d5
Josh Ellis <josh@imjoshellis.com>
2020-09-03 20:19:23 -0500

    Add column styling

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Item from './Item'
 import { Droppable } from 'react-beautiful-dnd'
+import { styled } from '../stiches.config'
 
 interface ColumnProps {
   col: {
@@ -9,33 +10,41 @@ interface ColumnProps {
   }
 }
 
+const StyledColumn = styled('div', {
+  padding: '24px 16px',
+  display: 'flex',
+  flexDirection: 'column',
+  marginTop: 8,
+
+  h2: {
+    margin: 0,
+    padding: '0 16px'
+  }
+})
+
+const StyledList = styled('div', {
+  backgroundColor: '#ddd',
+  borderRadius: 8,
+  padding: 16,
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+  marginTop: 8
+})
+
 const Column: React.FC<ColumnProps> = ({ col: { list, id } }) => {
   return (
     <Droppable droppableId={id}>
       {provided => (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: 120
-          }}
-        >
+        <StyledColumn>
           <h2>{id}</h2>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              minHeight: 120
-            }}
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
+          <StyledList {...provided.droppableProps} ref={provided.innerRef}>
             {list.map((text, index) => (
               <Item key={text} text={text} index={index} />
             ))}
             {provided.placeholder}
-          </div>
-        </div>
+          </StyledList>
+        </StyledColumn>
       )}
     </Droppable>
   )
